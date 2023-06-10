1b84ade2211fc2932d6ed8e54798333c83650ab5
João Otávio C. de Carvalho <joaootavioc.21@gmail.com>
2023-06-09 21:32:26 -0300

    added nodemon to dev mode

--- src/resolvers-and-services.ts
+++ src/resolvers-and-services.ts
@@ -3,8 +3,12 @@ import { UserLoginResolver } from './resolvers/login.resolver';
 import { AppUserService } from './services/appUser.service';
 import { UserLoginService } from './services/userLogin.service';
 import { TokenService } from './services/token.service';
-import { PrismaService } from 'prisma/prisma.service';
-
+import { PrismaService } from '../prisma/prisma.service';
 
 export const resolvers = [AppUserResolver, UserLoginResolver];
-export const services = [AppUserService, UserLoginService, TokenService, PrismaService];
+export const services = [
+  AppUserService,
+  UserLoginService,
+  TokenService,
+  PrismaService,
+];
